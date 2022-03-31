de5bd0fc3c5ef462159296fab6ad9d0ae1824c95
Drew Gonzales <drew.gonzales@datadoghq.com>
2022-03-30 22:20:41 -0700

    init

--- cmd/install.go
+++ cmd/install.go
@@ -2,13 +2,16 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/Masterminds/semver"
 	"github.com/urfave/cli/v2"
+
+	"github.com/drewgonzales360/goenv/pkg"
 )
 
 const (
-	InstallDirectory string = "/usr/local/go"
+	InstallDirectory string = "/usr/local/go/"
 )
 
 func Install(c *cli.Context) error {
@@ -17,14 +20,25 @@ func Install(c *cli.Context) error {
 		version = c.Args().Get(0)
 	}
 
-	_, err := semver.NewVersion(version)
+	goVersion, err := semver.NewVersion(version)
 	if err != nil {
-		return fmt.Errorf("could not parse version")
+		return fmt.Errorf("could not parse version as a semver")
 	}
 
-	// Download go tarball
+	downloadURL := pkg.FormatDownloadURL(*goVersion)
+	filePath, err := pkg.DownloadFile(downloadURL)
+	if err != nil {
+		return fmt.Errorf("could not download go")
+	}
 
-	// Untar
+	err = pkg.ExtractTarGz(filePath, InstallDirectory+goVersion.Original())
+	if err != nil {
+		return fmt.Errorf("could not extract go")
+	}
+
+	if err := os.Symlink("/usr/local/bin/go", InstallDirectory+goVersion.Original()+"/bin/go"); err != nil {
+		return fmt.Errorf("could not link go")
+	}
 
 	return nil
 }

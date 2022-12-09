8e2ab84927a23def0b2212ecc1eda57e3fc073fc
Drew Gonzales <drew.gonzales@datadoghq.com>
2022-12-09 17:00:46 -0500

    update license year

--- internal/cmd/use.go
+++ internal/cmd/use.go
@@ -1,5 +1,5 @@
 // ///////////////////////////////////////////////////////////////////////
-// Copyright 2022 Drew Gonzales
+// Copyright 2023 Drew Gonzales
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
